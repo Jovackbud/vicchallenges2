cfaeb434010a894e20628a01fd574ee3cc0d580e
jovackbud <jovack123456789@gmail.com>
2020-09-05 02:37:34 +1000

    first commit

--- question_4b.py
+++ question_4b.py
@@ -2,23 +2,27 @@
 
 from random import randint
 
-expected_input = randint(1, 1000)
-player_guess = int(input('Guess a number between 1 to 1000:'))
-counter = 0
-while player_guess != expected_input:
-    counter += 1
-    if player_guess < expected_input:
-        print('Too low, try again')
-    elif player_guess > expected_input:
-        print('Too high, try again')
-    player_guess = int(input('Guess a number between 1 to 1000:'))
-else:
-    pass
+def guess_game():
+	expected_input = randint(1, 1000)
+	player_guess = int(input('Guess a number between 1 to 1000:'))
+	counter = 0
+	while player_guess != expected_input:
+		counter += 1
+		if player_guess < expected_input:
+			print('Too low, try again')
+		elif player_guess > expected_input:
+			print('Too high, try again')
+		player_guess = int(input('Guess a number between 1 to 1000:'))
+	else:
+		pass
 
+def score_check():
+	if counter < 10:
+		print('Either you know the secret or you got lucky!')
+	elif counter > 10:
+		print('You should be able to do better!')
+	else:
+		print('Aha! You know the secret!')
+	guess_game()
 
-if counter < 10:
-    print('Either you know the secret or you got lucky!')
-elif counter > 10:
-    print('You should be able to do better!')
-else:
-    print('Aha! You know the secret!')
+guess_game()
