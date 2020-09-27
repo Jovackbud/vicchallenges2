314518b24b10d7dd5801bd6fbb09290135d89dce
jovackbud <jovack123456789@gmail.com>
2020-09-28 08:51:46 +1000

    completed

--- question_3.py
+++ question_3.py
@@ -1,10 +1,15 @@
 # calculating_distance
+from math import sqrt
+
+
 def calculating_distance(x1, y1, x2, y2):
-    result = (abs(x2 - x1), abs (y2 - y1))
+    x = (x2 - x1) ** 2
+    y = (y2 - y1) ** 2
+    result = sqrt(x + y)
     print(result)
 
 
-calculating_distance(int(input('Enter the first X cordinate: ')),
- int(input('Enter the first Y cordinate: ')),
-  int(input('Enter the second X cordinate: ')),
-   int(input('Enter the second Y cordinate: ')))
+calculating_distance(int(input('Enter the first X coordinate: ')),
+                     int(input('Enter the first Y coordinate: ')),
+                     int(input('Enter the second X cordinate: ')),
+                     int(input('Enter the second Y coordinate: ')))
