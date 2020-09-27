1ffa8f47215b0e6dcbd2c6526f0c7ae0eaeb055c
jovackbud <jovack123456789@gmail.com>
2020-09-28 08:54:13 +1000

    completed

--- question_3.py
+++ question_3.py
@@ -3,9 +3,9 @@ from math import sqrt
 
 
 def calculating_distance(x1, y1, x2, y2):
-    x = (x2 - x1) ** 2
-    y = (y2 - y1) ** 2
-    result = sqrt(x + y)
+    x_change = (x2 - x1) ** 2
+    y_change = (y2 - y1) ** 2
+    result = sqrt(x_change + y_change)
     print(result)
 
 
